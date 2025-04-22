97c72c0d9d2d29ca2afb6922b7e2be82d84b5988
Gina-4 <Georgina.Riri4@gmail.com>
2025-04-22 14:12:16 +1200

    updated

--- counter-creator/src/Counter.jsx
+++ counter-creator/src/Counter.jsx
@@ -1,23 +1,46 @@
+import {useState} from 'react';
 import React from 'react';
 function Counter() {
   // TODO: Implement useState hook here
-  const count = 0;
+  const [count, setCount] = useState (0);
 
   // TODO: Implement increment function
   const increment = () => {
-    console.log('Increment function not implemented yet');
+   setCount(count + 1)
+   // - Implement a max value that the counter cannot exceed.
+   if(count >= 5) {
+    setCount(count)
+   }
   };
 
   // TODO: Implement decrement function
   const decrement = () => {
-    console.log('Decrement function not implemented yet');
+    if (count > 0) {
+      setCount(count - 1);
+    }
   };
-
+// - Add a reset button that sets the counter back to 0.
+const reset = () => {
+  setCount(0)
+}
+// - Skip certain numbers (skip 5s)
+const skipNumbers = () => {
+  {
+    if(count % 5 === 4) {
+      setCount(count + 2)
+    }
+    else (
+    setCount(count + 1))
+    
+  }
+}
   return (
     <div className="counter">
-      <h2>Count: {count}</h2>
+      <h2>Counter: {count}</h2>
       <button onClick={decrement}>-</button>
       <button onClick={increment}>+</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={skipNumbers}>skip number</button>
     </div>
   );
 }
